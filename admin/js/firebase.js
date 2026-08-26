bca11caebec318e06d0a9efda574baf39d1ab9f8
thearjundev <arjunkumargaud00@gmail.com>
2026-08-25 23:28:40 -0700

    Initialize Firebase app with Auth and Firestore

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -1,1 +1,29 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVMATK68o-lLNI2ESxY2FTvrtwiPBeTRY",
+  authDomain: "zuzoo-6afa5.firebaseapp.com",
+  projectId: "zuzoo-6afa5",
+  storageBucket: "zuzoo-6afa5.firebasestorage.app",
+  messagingSenderId: "560745524336",
+  appId: "1:560745524336:web:3db8ae577ccb65456598b5",
+  measurementId: "G-RR5MN45J4S"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
